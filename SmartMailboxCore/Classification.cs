bf100a067eae344a2113d748047899d8878b37a1
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2019-01-19 23:33:57 +0000

    Add missing predictor, classification

---
+++ SmartMailboxCore/Classification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMailbox
+{
+    public struct Classification
+    {
+        public bool isSpam { get;  set; }
+    }
+}
