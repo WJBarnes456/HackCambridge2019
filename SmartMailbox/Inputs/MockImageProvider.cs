d6107ec97c0662656556916789adb2baeb471d33
Will Barnes <39161512+WJBarnes456@users.noreply.github.com>
2019-01-19 21:19:41 +0000

    Add input functionality

---
+++ SmartMailbox/Inputs/MockImageProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMailbox.Inputs
+{
+    // Mock image provider -- always returns the given filename, useful for testing
+    class MockImageProvider : IImageProvider
+    {
+        private string filename;
+
+        public MockImageProvider(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public string TakeImage()
+        {
+            // Assume the filename we were originally given already exists
+            return filename;
+        }
+    }
+}
